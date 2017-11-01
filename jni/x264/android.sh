2cb0026c31d27c3046c239f939a5f5d1f191c7c7
ring0 <2237026327@qq.com>
2017-11-02 00:06:22 +0800

    [编译]重编译 x264, 使可以被链接到 ffmpeg 中

--- jni/x264/android.sh
+++ jni/x264/android.sh
@@ -3,21 +3,27 @@
 #      author: zd
 #      2015-2016 iexin
 #########################################################################
-export NDK=/home/ring0/develop/android-ndk-r14b
+export NDK=/home/ring0/develop/android-ndk-r10d
 export HOST=
 export CROSS=
 export SYSROOT=
 export PREFIX=
 export CC_FLAGS=
 export PREFIX_LIB=
+export GCC_LIB=
+export LD=
+export DISABLE_ASM=
 
 __internal_build_env_armeabi_v5a(){
       PREFIX=android/armeabi
       HOST=arm-linux-androideabi
       CROSS=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-arm
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-arm/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-arm/usr/lib
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated -marm -march=arm -fvisibility=hidden
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_env_armeabi_v7a(){
@@ -25,8 +31,11 @@ __internal_build_env_armeabi_v7a(){
       HOST=arm-linux-androideabi
       CROSS=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-arm
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-arm/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-arm/usr/lib
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -mfpu=neon -marm -march=arm -fvisibility=hidden
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_env_aarch64_v8a(){
@@ -34,8 +43,11 @@ __internal_build_env_aarch64_v8a(){
       HOST=aarch64-linux-android
       CROSS=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-arm64
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-arm64/usr/lib
-      CC_FLAGS=-march=armv8 -D__ANDROID__ -D__ARM_ARCH_8__ -D__ARM_ARCH_8A__
+      PREFIX_LIB=$NDK/platforms/android-21/arch-arm64/usr/lib
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
+      CC_FLAGS=-fPIC -DANDROID -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -mfpu=neon -marm -march=arm64 -fvisibility=hidden
+      DISABLE_ASM=""
 }
 
 __internal_build_env_intel_x86(){
@@ -43,8 +55,11 @@ __internal_build_env_intel_x86(){
       HOST=i686-linux-android
       CROSS=$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-x86
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-x86/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-x86/usr/lib
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated -mssse3 -mavx -fvisibility=hidden
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_env_intel_x86_64(){
@@ -52,8 +67,11 @@ __internal_build_env_intel_x86_64(){
       HOST=x86_64-linux-android
       CROSS=$NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-x86_64
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-x86_64/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-x86_64/usr/lib64
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated -mssse3 -mavx -fvisibility=hidden
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_env_mips32(){
@@ -61,8 +79,11 @@ __internal_build_env_mips32(){
       HOST=mipsel-linux-android
       CROSS=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-mips
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-mips/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-mips/usr/lib
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_env_mips64(){
@@ -70,30 +91,47 @@ __internal_build_env_mips64(){
       HOST=mips64el-linux-android
       CROSS=$NDK/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/$HOST-
       SYSROOT=$NDK/platforms/android-21/arch-mips64
-      PREFIX_LIB=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-mips64/usr/lib
+      PREFIX_LIB=$NDK/platforms/android-21/arch-mips64/usr/lib64
+      LD=$CROSS"ld"
+      GCC_LIB=$NDK/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/lib/gcc/$HOST/4.9/libgcc.a
       CC_FLAGS=-fPIC -DANDROID -D__thumb__ -Wfatal-errors -Wno-deprecated -mhard-float -mmsa -mdsp -mdspr2
+      DISABLE_ASM="--disable-asm"
 }
 
 __internal_build_general(){
       ./configure \
-            --prefix=$PREFIX \
-            --host=$HOST \
-            --sysroot=$SYSROOT \
-            --cross-prefix=$CROSS \
+            --prefix=$PREFIX         \
+            --host=$HOST             \
+            --sysroot=$SYSROOT       \
+            --cross-prefix=$CROSS    \
             --extra-cflags=$CC_FLAGS \
-            --extra-ldflags="-nostdlib" \
-            --enable-static \
-            --enable-shared \
-            --enable-strip \
-            --disable-asm \
-            --disable-cli \
-            --disable-win32thread \
-            --disable-avs \
-            --disable-swscale \
-            --disable-lavf \
-            --disable-ffms \
-            --disable-gpac \
+            $DISABLE_ASM             \
+            --enable-static          \
+            --enable-strip           \
+            --enable-pic             \
+            --disable-cli            \
+            --disable-win32thread    \
+            --disable-avs            \
+            --disable-swscale        \
+            --disable-lavf           \
+            --disable-ffms           \
+            --disable-gpac           \
             --disable-lsmash
+}
+
+__internal_build_shread(){
+      $LD -rpath-link=-$PREFIX_LIB   \
+            -L$PREFIX_LIB            \
+            -soname libx264.so       \
+            -shared                  \
+            -nostdlib                \
+            -Bsymbolic               \
+            --whole-archive          \
+            --no-undefined           \
+            -o $PREFIX/libx264.so    \
+            $PREFIX/lib/libx264.a    \
+            -lc -lm -lz -ldl -llog   \
+            --dynamic-linker=/system/bin/linker $GCC_LIB
 }
 
 build_armeabi_v5a(){
@@ -101,6 +139,7 @@ build_armeabi_v5a(){
       __internal_build_env_armeabi_v5a
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_armeabi_v7a(){
@@ -108,6 +147,7 @@ build_armeabi_v7a(){
       __internal_build_env_armeabi_v7a
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_aarch64_v8a(){
@@ -115,6 +155,7 @@ build_aarch64_v8a(){
       __internal_build_env_aarch64_v8a
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_intel_x86(){
@@ -122,6 +163,7 @@ build_intel_x86(){
       __internal_build_env_intel_x86
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_intel_x86_64(){
@@ -129,6 +171,7 @@ build_intel_x86_64(){
       __internal_build_env_intel_x86_64
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_mips32(){
@@ -136,6 +179,7 @@ build_mips32(){
       __internal_build_env_mips32
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_mips64(){
@@ -143,6 +187,7 @@ build_mips64(){
       __internal_build_env_mips64
       __internal_build_general
       make -j4 install
+      __internal_build_shread
 }
 
 build_armeabi_v5a
--- jni/x264/config.mak
+++ jni/x264/config.mak
@@ -6,18 +6,18 @@ libdir=${exec_prefix}/lib
 includedir=${prefix}/include
 SYS_ARCH=MIPS
 SYS=LINUX
-CC=/home/ring0/develop/android-ndk-r14b/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc
-CFLAGS=-Wno-maybe-uninitialized -Wshadow -O3 -ffast-math  -Wall -I. -I$(SRCPATH) --sysroot=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-mips64  -std=gnu99 -D_GNU_SOURCE -fPIC -fomit-frame-pointer -fno-tree-vectorize
+CC=/home/ring0/develop/android-ndk-r10d/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc
+CFLAGS=-Wno-maybe-uninitialized -Wshadow -O3 -ffast-math  -Wall -I. -I$(SRCPATH) --sysroot=/home/ring0/develop/android-ndk-r10d/platforms/android-21/arch-mips64  -std=gnu99 -D_GNU_SOURCE -fPIC -fomit-frame-pointer -fno-tree-vectorize
 COMPILER=GNU
 COMPILER_STYLE=GNU
 DEPMM=-MM -g0
 DEPMT=-MT
-LD=/home/ring0/develop/android-ndk-r14b/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc -o 
-LDFLAGS= --sysroot=/home/ring0/develop/android-ndk-r14b/platforms/android-21/arch-mips64 -nostdlib -lm -s -ldl
+LD=/home/ring0/develop/android-ndk-r10d/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc -o 
+LDFLAGS= --sysroot=/home/ring0/develop/android-ndk-r10d/platforms/android-21/arch-mips64 -lm  -s -ldl
 LIBX264=libx264.a
-AR=/home/ring0/develop/android-ndk-r14b/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc-ar rc 
-RANLIB=/home/ring0/develop/android-ndk-r14b/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc-ranlib
-STRIP=/home/ring0/develop/android-ndk-r14b/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-strip
+AR=/home/ring0/develop/android-ndk-r10d/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc-ar rc 
+RANLIB=/home/ring0/develop/android-ndk-r10d/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-gcc-ranlib
+STRIP=/home/ring0/develop/android-ndk-r10d/toolchains/mips64el-linux-android-4.9/prebuilt/linux-x86_64/bin/mips64el-linux-android-strip
 INSTALL=install
 AS=
 ASFLAGS= -I. -I$(SRCPATH) -DSTACK_ALIGNMENT=4 -DPIC -DHIGH_BIT_DEPTH=0 -DBIT_DEPTH=8
@@ -31,11 +31,6 @@ PROF_GEN_LD=-fprofile-generate
 PROF_USE_CC=-fprofile-use
 PROF_USE_LD=-fprofile-use
 HAVE_OPENCL=yes
-SOSUFFIX=so
-SONAME=libx264.so
-SOFLAGS=-shared -Wl,-soname,$(SONAME)  -Wl,-Bsymbolic
-default: lib-shared
-install: install-lib-shared
 default: lib-static
 install: install-lib-static
 LDFLAGSCLI = 
